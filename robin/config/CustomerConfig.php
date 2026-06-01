1b9b37b5f816ac9a0d443f31705fdf082699c3d7
RobinSoup <robin.wagelmans@edu-kw1c.nl>
2026-06-01 14:10:19 +0200

    database connectie customers pagina

---
+++ robin/config/CustomerConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "gamevault";
+$servername = "localhost";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
