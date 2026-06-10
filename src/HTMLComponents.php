8a25373c614f67db1ace48bf83f1efc61d4a6884
Greygar724665 <lars.vandermeijs@edu-kw1c.nl>
2026-06-10 11:18:05 +0200

    update NavBar method in HTMLComponents class to accept dynamic navigation links

--- src/HTMLComponents.php
+++ src/HTMLComponents.php
@@ -6,18 +6,32 @@
  */
 class HTMLComponents
 {
-    public static function NavBar(): string {
-		
-		
-		
-		return
-		<<<HTML
+	/**
+	 * @param array<string, string> $dirs Array of [pageName => href] pairs.
+	 * @return string HTML Navbar
+	 */
+    public static function NavBar(array $dirs = []): string {
+		$html = <<<HTML
 		<nav aria-label="Main Navigation" class="main-navbar">
-		    <a href="#">Home</a>
-		    <a href="#">Test1</a>
-		    <a href="#">Test2</a>
-		    <a href="#">Test3</a>
-		</nav>
 		HTML;
+		$isAssociative = !array_is_list($dirs);
+		
+		foreach (array_keys($dirs) as $dirKey) {
+			$pageName = $isAssociative
+				? $dirKey
+				: $dirs[$dirKey]; // $dirKey is an inex is the array is a list.
+			$href = ($isAssociative
+				? (!empty($dirs[$dirKey]) ? $dirs[$dirKey] : '#') // If href is an empty string, then '#'
+				: '#')
+				?? '#'; // If null then '#'
+			
+			$html .= <<<HTML
+				<a href="$href">$pageName</a>
+			HTML;
+			
+		}
+		
+		$html .= "</nav>";
+		return $html;
 	}
 }
